3409134a9ef05e199673818ac90a8252f9a85387
TEST Ball <160318630+ishakmusk2027@users.noreply.github.com>
2024-08-10 20:10:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const Discord = require('discord.js-selfbot-v13');
-const express = require('express');
-require('dotenv').config();
-
-// إعداد خادم Express
-const app = express();
-const PORT = process.env.PORT || 2000;
-
-app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
 });
+//environment
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send(`
-    <body>
-      <center><h1>Bots 24H ON!</h1></center>
-    </body>`);
-});
-
-// دالة لتنسيق الوقت
 function formatTime() { 
   const date = new Date();
   const options = {
@@ -28,39 +16,40 @@ function formatTime() {
   };
   return new Intl.DateTimeFormat('en-US', options).format(date);
 }
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+client.on('ready', async () => {
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`);
 
-// توكنات البوتات
-const tokens = [process.env.TOKEN1, process.env.TOKEN2, process.env.TOKEN3];
+  const r = new Discord.RichPresence()
+    .setApplicationId('1271896737241366682')
+    .setType('PLAYING')
+    .setURL('https://discord.gg/vPGJngQbUx') 
+    .setState('Hero')
+    .setName('Hero On Top')
+    .setDetails(`Join Server`)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1271463274537291920/1271640687267287070/snapedit_1723122031051-removebg-preview_3.webp?ex=66b8bbed&is=66b76a6d&hm=6e9104c5b95c2580a456065accd09ff108e1004236e2dce298557418ef3b7bf6&=&format=webp') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Hero On Top') 
+    .setAssetsSmallImage('https://cdn3.emoji.gg/emojis/8409-nitro.png') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Hero Server') 
+    .addButton('Join Now', 'https://discord.gg/vPGJngQbUx');
 
-// إعداد البوتات
-tokens.forEach(token => {
-  const client = new Discord.Client({
-    readyStatus: false,
-    checkUpdate: false
-  });
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
 
-  client.on('ready', async () => {
-    console.clear();
-    console.log(`${client.user.tag} - rich presence started!`);
-
-    const r = new Discord.RichPresence()
-      .setApplicationId('1271896737241366682')
-      .setType('PLAYING')
-      .setURL('https://discord.gg/vPGJngQbUx')
-      .setState('Hero')
-      .setName('Hero On Top')
-      .setDetails('Join Server')
-      .setStartTimestamp(Date.now())
-      .setAssetsLargeImage('https://media.discordapp.net/attachments/1271463274537291920/1271637467954548736/39632582222_6.webp?ex=66b8b8ee&is=66b7676e&hm=b9a2561f21ad0a6cea7b4ae9abae940a6feac54eff1b27a01afbb9732befb3fc&=&format=webp')
-      .setAssetsLargeText('Hero On Top')
-      .setAssetsSmallImage('https://cdn3.emoji.gg/emojis/8409-nitro.png')
-      .setAssetsSmallText('Hero Server')
-      .addButton('Join', 'https://discord.gg/vPGJngQbUx');
-
-    client.user.setActivity(r);
-    client.user.setPresence({ status: "idle" }); // dnd, online, idle, offline
-  });
-
-  client.login(token);
 });
 
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
